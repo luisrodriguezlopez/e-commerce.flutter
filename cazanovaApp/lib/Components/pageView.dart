07a86a5d91b24057b07a0ffc64f75a73bcb27e66
Luis Rodríguez <luuiz.rodriguez.lopez@gmail.com>
2020-10-28 22:58:35 -0700

    banners , bottomBar y interacciones

---
+++ cazanovaApp/lib/Components/pageView.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CarrouselView extends StatelessWidget {
+  const CarrouselView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Sliders(),
+    );
+  }
+}
+
+class Sliders extends StatelessWidget {
+  const Sliders({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.brown,
+        child: ListView(
+            scrollDirection: Axis.horizontal,
+            physics: PageScrollPhysics(), // this is what you are looking for
+            children: [
+              Row(children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 2,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 2,
+                  height: 100,
+                  color: Colors.pink,
+                )
+              ]),
+            ]));
+  }
+}
